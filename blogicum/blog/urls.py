1b0931e9c862de37f59f9d9221b3da3e810fffc1
MuUJa <mihail.podyachih@gmail.com>
2025-01-09 13:13:31 +0300

    Fix format and localization

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,7 +6,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path(
+        'posts/<int:pk>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
     path(
         'category/<slug:slug>/',
         views.CategoryView.as_view(),
