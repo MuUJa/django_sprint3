d4f98277be97c9c48e08017bce3eb35eb57c09da
MuUJa <mihail.podyachih@gmail.com>
2025-01-09 11:34:32 +0300

    Copy from django1 task

---
+++ blogicum/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def index(request):
+    template = 'blog/index.html'
+    context = {'posts': reversed(posts)}
+    return render(request, template, context)
+
+
+def post_detail(request, id):
+    template = 'blog/detail.html'
+    context = {'post': posts_dict[id]}
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    context = {'category_slug': category_slug}
+    return render(request, template, context)
